291cbc7110662f5d14919602377ce709f1902d75
vanillaturtlechips <2220110150@office.kopo.ac.kr>
2026-02-16 21:48:50 +0900

    edit alb

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,12 @@
+# 기존 백엔드 아래에 프론트엔드도 추가해주세요
+resource "aws_ecr_repository" "frontend" {
+  name                 = "silver-guardian-frontend"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+}
+
 resource "aws_ecr_repository" "backend" {
   name                 = "silver-guardian-backend"
   image_tag_mutability = "MUTABLE"
-  force_delete         = true # 테라폼 삭제 시 이미지 있어도 강제 삭제
+  force_delete         = true
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,30 +1,41 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "5.5.1"
 
   name = "silver-guardian-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-northeast-2a", "ap-northeast-2c"]
-  
-  # 1. Private Subnet (EKS 앱용)
+  azs = ["ap-northeast-2a", "ap-northeast-2c"]
+
+  # 1. Private Subnets (워커 노드 및 내부 리소스용)
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"] 
   
-  # 2. Public Subnet (Bastion, 로드밸런서용)
+  # 2. Public Subnets (인터넷 게이트웨이와 연결된 로드밸런서용)
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"] 
 
-  # 3. Database Subnet (RDS, Redis용) - 이 부분이 추가되었습니다!
-  database_subnets = ["10.0.51.0/24", "10.0.52.0/24"]
+  # 3. Database Subnets (DB/Cache 전용)
+  database_subnets             = ["10.0.51.0/24", "10.0.52.0/24"]
   create_database_subnet_group = true
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  # 인터넷 연결 설정
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
 
+
   public_subnet_tags = {
+    # "이 서브넷은 '인터넷용(Public) 로드밸런서'를 만드는 곳이다"라고 AWS에 알림
     "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/silver-guardian" = "shared"
   }
+
   private_subnet_tags = {
+    # "이 서브넷은 '내부용(Internal) 로드밸런서'를 만드는 곳이다"라고 AWS에 알림
     "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/silver-guardian" = "shared"
+  }
+
+  tags = {
+    "kubernetes.io/cluster/silver-guardian" = "shared"
   }
 }
